d3451845dd6adea14e616cad43bba7909815015b
ymat2 <ymat2@users.noreply.github.com>
2024-10-04 19:32:27 +0900

    :fire: Move rstat to scRibble and Remove bio

--- bio/raxml-ng.sh
+++
@@ -1,7 +0,0 @@
-raxml-ng --msa Obscn.fa --all --model GTR+G --bs-trees 100 --threads 2
-
-# --msa	<alignment file>
-# --all	topology search + bootstrapping
-# --model	specify a model
-# --bs-trees	number of bootstrapping
-# --threads	number of threads
