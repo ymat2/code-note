04d50e195b1980c2bbce204bc17bb67a4adec86f
ymat2 <ymat2@users.noreply.github.com>
2024-10-07 01:20:18 +0900

    :pen: Learn argument parse in Go

---
+++ go/argparse.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"flag"  // package to parse command-line arguments
+)
+
+func main() {
+	// use flag.Type("Flag name", "Default value", "Help message")
+  var name = flag.String("name", "Yuki", "Your name")
+
+	var age int
+	// or bind to variable using flag.TypeVar(%var, "Flag name", "Default value", "Help message")
+	flag.IntVar(&age, "age", 25, "Your age")
+
+	flag.Parse()
+
+  // use pointer (*) in flagType, value in TypeVar
+	fmt.Println(*name, age)
+}
